6f4166e6f65fe6295db67542427538d5a420c7d4
kelson.gardner <kelson.gardner14@gmail.com>
2024-07-02 16:49:40 -0600

    Fixed incorrect require statement in the Square class

--- src/main/kotlin/Square.kt
+++ src/main/kotlin/Square.kt
@@ -4,9 +4,6 @@ class Square(
     private val pointB: Point
 ) : Rectangle(pointA, pointB) {
     init{
-        require(!pointA.getValues().contentEquals(pointB.getValues())){
-            "The point values for the rectangle cannot be the same!"
-        }
         require(abs(pointA.getXValue() - pointB.getXValue())
                 == abs(pointA.getYValue() - pointB.getYValue())){
             "All sides must be the same length!"
